8057eacc96c512e6724b9135c8cfeb7ad4880bf7
angelp495 <angeljpa95@gmail.com>
2021-05-10 19:58:00 -0500

    Link y NavLink

---
+++ react-router/src/components/MenuConceptos.js
@@ -0,0 +1,37 @@
+import { Link, NavLink } from "react-router-dom";
+
+const MenuConceptos = () => {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <span>Enlaces html (no recomendado): </span>
+          <a href="/">Home</a>
+          <a href="/acerca">Acerca</a>
+          <a href="/contacto">Contacto</a>
+        </li>
+        <li>
+          <span>Componente Link: </span>
+          <Link to="/">Home</Link>
+          <Link to="/acerca">Acerca</Link>
+          <Link to="/contacto">Contacto</Link>
+          <Link to="/no-existe">Error 404</Link>
+        </li>
+        <li>
+          <span>Componente NavLink: </span>
+          <NavLink to="/" exact activeClassName="active">
+            Home
+          </NavLink>
+          <NavLink to="/acerca" exact activeClassName="active">
+            Acerca
+          </NavLink>
+          <NavLink to="/contacto" exact activeClassName="active">
+            Contacto
+          </NavLink>
+        </li>
+      </ol>
+    </nav>
+  );
+};
+
+export default MenuConceptos;
