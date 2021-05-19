3e46bc5a05048cd94393f20b286bf14ed530c6ce
angelp495 <angeljpa95@gmail.com>
2021-05-19 14:24:38 -0500

    Hash Router

--- react-router/src/components/ConceptosBasicos.js
+++ react-router/src/components/ConceptosBasicos.js
@@ -1,5 +1,6 @@
 import {
   BrowserRouter as Router,
+  HashRouter,
   Redirect,
   Route,
   Switch,
@@ -19,6 +20,29 @@ import PrivateRoute from "./PrivateRoute";
 export const ConceptosBasicos = () => {
   return (
     <div>
+      <h2>Hash Router</h2>
+      <HashRouter>
+        <MenuConceptos />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/acerca" component={Acerca} />
+          <Route exact path="/contacto" component={Contacto} />
+          <Route exact path="/usuario/:username" component={Usuario} />
+          <Route exact path="/productos" component={Productos} />
+          <Route exact path="/about">
+            <Redirect to="/acerca" />
+          </Route>
+          <Route exact path="/contact">
+            <Redirect to="/contacto" />
+          </Route>
+          <Route exact path="/login" component={Login} />
+          {/* <Route exact path="/dashboard" component={Dashboard} /> */}
+          <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          <Route path="/react" component={ReactTopics} />
+          <Route path="*" component={Error404} />
+        </Switch>
+      </HashRouter>
+      <hr />
       <h2>Conceptos Básicos</h2>
       <Router>
         <MenuConceptos />
